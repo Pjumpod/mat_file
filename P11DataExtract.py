aac05f431fb45cc56239a7fa10c0ebe2a1a58ec1
Jumpod Plekhongthu <pjumpod@gmail.com>
2024-07-30 09:42:04 +0700

    Add files via upload

---
+++ P11DataExtract.py
@@ -0,0 +1,45 @@
+import os
+import zipfile
+import pandas as pd
+
+# Define the base directory containing subdirectories with common folder
+base_dir = r'\\fab2-fs13.luminartech.com\Departments\advmfg\P1-1'
+outputpath = r'C:\output'
+if not os.path.exists(outputpath):
+  os.makedirs(outputpath)
+# Create an empty DataFrame to hold the combined data
+combined_df = pd.DataFrame()
+
+# Walk through the directory structure to find 'FULL_CALIBRATION' folders
+for root, dirs, files in os.walk(base_dir):
+    # Only proceed if the current directory contains 'FULL_CALIBRATION'
+    if 'FULL_CALIBRATION' in dirs:
+        full_calibration_dir = os.path.join(root, 'FULL_CALIBRATION')
+        
+        # Process each ZIP file in the 'FULL_CALIBRATION' directory
+        for file in os.listdir(full_calibration_dir):
+            if file.endswith('.zip'):
+                zip_path = os.path.join(full_calibration_dir, file)
+                zip_name = os.path.splitext(file)[0]  # Extract ZIP file name without extension
+                print("==> " + zip_path)
+                try:
+                    # Extract ZIP file
+                    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                        if 'irisDatabaseRecord.csv' in zip_ref.namelist():
+                            # Read the CSV file from the ZIP
+                            with zip_ref.open('irisDatabaseRecord.csv') as my_file:
+                                df = pd.read_csv(my_file)
+                                
+                                # Add a new column with the ZIP file name
+                                df['ZipFolderName'] = zip_name
+                                
+                                # Append to the combined DataFrame
+                                combined_df = pd.concat([combined_df, df], ignore_index=True)
+                except Exception as e:
+                    print(f"Error processing ZIP file {zip_path}: {e}")
+
+# Save the combined data to a new CSV file
+output_csv_path = os.path.join(outputpath,"combined_data_P11.csv")
+combined_df.to_csv(output_csv_path, index=False)
+
+print(f"Combined data saved to {output_csv_path}")
